ad30524fef0168b4fc5ee6655f895d8f071c3a64
sandeep <ankurmall89@gmail.com>
2018-12-22 18:02:10 +0530

    Conneecting elastic search with spring boot

---
+++ src/main/java/com/javasks/elastic/ElasticExampleApplication.java
@@ -0,0 +1,12 @@
+package com.javasks.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javasks/elastic/ElasticExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javasks.elastic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
